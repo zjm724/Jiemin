c77610ef517fb1073f286997b4522cdf2f5b8f7a
zjm724 <zjm724@hotmail.com>
2017-08-18 16:48:46 -0700

    Project subsection good for now.
    Good for now. Will add more projects later.

--- app/src/main/java/com/jiemin/zjm72/jiemin/fragment_project.java
+++ app/src/main/java/com/jiemin/zjm72/jiemin/fragment_project.java
@@ -6,15 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
- * Created by zjm72 on 8/11/2017.
+ * Created by zjm72 on 8/14/2017.
  */
 
 public class fragment_project extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.content_about_me, container, false);
-        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.content_projects, container, false);
     }
+}
