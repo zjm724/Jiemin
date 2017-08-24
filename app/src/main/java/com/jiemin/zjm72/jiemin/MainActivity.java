759c9a43e395bc4debbd628280751753f4c0d635
zjm724 <zjm724@hotmail.com>
2017-08-24 08:54:53 -0700

    On process

--- app/src/main/java/com/jiemin/zjm72/jiemin/MainActivity.java
+++ app/src/main/java/com/jiemin/zjm72/jiemin/MainActivity.java
@@ -151,6 +151,28 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_educations) {
 
+            // Handle click "projects" action
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Educations");
+
+
+
+            // Create a new fragment and specify the planet to show based on position
+            fragmentClass = fragment_education.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+            // Highlight the selected item, update the title, and close the drawer
+
         } else if (id == R.id.nav_technical_skills) {
 
         } else if (id == R.id.nav_just_for_fun) {
---
+++ app/src/main/java/com/jiemin/zjm72/jiemin/fragment_education.java
@@ -0,0 +1,22 @@
+package com.jiemin.zjm72.jiemin;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zjm72 on 8/18/2017.
+ */
+
+public class fragment_education extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.content_educations, container, false);
+    }
+}
